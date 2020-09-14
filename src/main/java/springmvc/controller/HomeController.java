9b9598462cad70de6a009bb072c30535d1014cd6
karun <karunahuja1998@gmail.com>
2020-09-14 06:56:11 +0530

    Sending data from controller to views with model.addAttribute()

--- src/main/java/springmvc/controller/HomeController.java
+++ src/main/java/springmvc/controller/HomeController.java
@@ -1,6 +1,10 @@
 package springmvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,11 +12,23 @@ public class HomeController {
 	
 	
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
 		System.out.println("Home url");
+		model.addAttribute("name", "Karun Ahuja");
+		model.addAttribute("id","50215");
+		
+		List<String> friends=new ArrayList<String>();
+		friends.add("Karun");
+		friends.add("Ahuja");
+		friends.add("Abc");
+		model.addAttribute("f", friends);
+		
 		return "index";
 		
 	}
+	
+	
+	 
 	
 	@RequestMapping("/about")
 	public String about() {
@@ -20,6 +36,8 @@ public class HomeController {
 		return "about";
 		
 	}
+	
+	
 	
 	
 
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,7 @@
+<%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,6 +12,24 @@
 
 <h1>Home Page</h1>
 <h1>Called by home controller</h1>
+
+<%
+String name=(String) request.getAttribute("name");
+String id=(String) request.getAttribute("id");
+List<String> friends=(List<String>) request.getAttribute("f");
+
+%>
+
+
+<h1>Name is <%=name %></h1>
+<h1>Id is <%=id %></h1>
+
+<%
+for(String s:friends){
+	out.println(s);
+}
+
+%>
 
 </body>
 </html>
