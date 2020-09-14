a310e25769608119326022eafd722462193d637d
karun <karunahuja1998@gmail.com>
2020-09-14 07:26:23 +0530

    ModelAndView | Sending Data from controller to View

--- src/main/java/springmvc/controller/HomeController.java
+++ src/main/java/springmvc/controller/HomeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -34,6 +35,19 @@ public class HomeController {
 	public String about() {
 		System.out.println("About url");
 		return "about";
+		
+	}
+	
+	
+	
+	@RequestMapping("/help")
+	public ModelAndView help() {
+		System.out.println("Help url");
+		ModelAndView modelAndView=new ModelAndView();
+		modelAndView.addObject("name","Karun");
+		modelAndView.setViewName("help");
+		
+		return modelAndView;
 		
 	}
 	
